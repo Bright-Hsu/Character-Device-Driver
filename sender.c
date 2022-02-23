56625f032e90ad36f1555bc8ab315fc5e67c1349
bright <2191579187@qq.com>
2022-02-23 21:41:33 +0800

    Create sender.c

---
+++ sender.c
@@ -0,0 +1,42 @@
+#include<sys/stat.h>
+#include<signal.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<string.h>
+#define _WRITE 0
+#define _READ  1
+
+int flag = _WRITE;
+
+int main()
+{
+    int fd = open("/dev/chardev0",O_RDWR,S_IRUSR | S_IWUSR);
+    char msg[101];
+    if(fd != -1)
+    {
+        while(1)
+        {
+        ROTATE:
+            if(flag == _WRITE)
+            {
+                printf("Process %d, Please send a message:",getpid());
+                fgets(msg,101,stdin);
+                write(fd,msg,strlen(msg));
+                flag = _READ;
+            }
+            else
+            {
+                memset(msg,0,101);
+                read(fd,msg,101);
+                if(strlen(msg) == 0)
+                    goto ROTATE;
+                printf("Message read from another process:%s",msg);
+                flag = _WRITE;
+            }
+            sleep(1);
+        }
+    }
+    else
+        return -1;
+}
